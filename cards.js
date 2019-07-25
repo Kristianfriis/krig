6a4884ce087d127860fd44a4522a7f3781a36793
Kristianfriis <47473621+Kristianfriis@users.noreply.github.com>
2019-07-25 17:49:42 +0200

    Initial commit

---
+++ cards.js
@@ -0,0 +1,8 @@
+var C1 = {
+  value: 14,
+  image: "https://i.imgur.com/XjSkqaf.png"
+}
+var C2 = {
+  value: 2,
+  image: "https://i.imgur.com/GFPY03X.png"
+}
---
+++ logic.js
@@ -0,0 +1,18 @@
+var cards = ["C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","D11","D12","D13","H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","H11","H12","H13","S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11","S12","S13"];
+var player = [];
+var computer = [];
+
+function dealer() {
+  for (var i = 0; i <= 25; i++) {
+    var random = Math.floor(Math.random() * cards.length);
+    player.push(cards[random]);
+    cards.splice(random, 1);
+    random = Math.floor(Math.random() * cards.length);
+    computer.push(cards[random]);
+    cards.splice(random, 1);
+  }
+}
+function tester() {
+  document.getElementById("computercardpic").src = C2.image;
+  document.getElementById("playercardpic").src = C2.image;
+}
