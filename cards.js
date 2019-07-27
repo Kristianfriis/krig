8933a79d60df2c8cb29cd59ad0256562740c9f84
Kristianfriis <47473621+Kristianfriis@users.noreply.github.com>
2019-07-27 14:33:08 +0200

    fixed cards and clubs db

--- cards.js
+++ cards.js
@@ -1,8 +1,52 @@
 var C1 = {
   value: 14,
-  image: "/cards/ace_of_clubs.png"
+  image: "/pic/ace_of_clubs.png"
 }
 var C2 = {
   value: 2,
-  image: "https://i.imgur.com/GFPY03X.png"
+  image: "/pic/2_of_clubs.png"
+}
+var C3 = {
+  value: 3,
+  image: "/pic/3_of_clubs.png"
+}
+var C4 = {
+  value: 4,
+  image: "/pic/4_of_clubs.png"
+}
+var C5 = {
+  value: 5,
+  image: "/pic/5_of_clubs.png"
+}
+var C6 = {
+  value: 6,
+  image: "/pic/6_of_clubs.png"
+}
+var C7 = {
+  value: 7,
+  image: "/pic/7_of_clubs.png"
+}
+var C8 = {
+  value: 8,
+  image: "/pic/8_of_clubs.png"
+}
+var C9 = {
+  value: 9,
+  image: "/pic/9_of_clubs.png"
+}
+var C10 = {
+  value: 10,
+  image: "/pic/10_of_clubs.png"
+}
+var C11 = {
+  value: 11,
+  image: "/pic/jack_of_clubs.png"
+}
+var C12 = {
+  value: 12,
+  image: "/pic/queen_of_clubs.png"
+}
+var C13 = {
+  value: 13,
+  image: "/pic/king_of_clubs.png"
 }
